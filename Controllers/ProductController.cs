0ca612ce2bbf72119f3fc8377a7e8f667ee18e36
JakubPawlowski99 <56391766+JakubPawlowski99@users.noreply.github.com>
2024-03-25 11:51:11 +0100

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using CRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> products = new List<Product>();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(products);
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            products.Add(product);
+            return RedirectToAction("Index");
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var product = products.Find(p => p.Id == id);
+            return View(product);
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Product product)
+        {
+            var index = products.FindIndex(p => p.Id == id);
+            if (index != -1)
+            {
+                products[index] = product;
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var product = products.Find(p => p.Id == id);
+            return View(product);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            products.RemoveAll(p => p.Id == id);
+            return RedirectToAction("Index");
+        }
+    }
+}
